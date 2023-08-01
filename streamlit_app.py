23764fcf419b5a2f9392cbb25e4cea52620a6835
Arthur C <78846238+treize-khushrenada@users.noreply.github.com>
2023-08-01 16:34:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,79 +17,85 @@ def get_data():
 
     try:
         response1 = response1['data'][0]['eta']
-        response1 = datetime.fromisoformat(response1)
-        response1 = (response1 - hkt_now)
+        eta = datetime.fromisoformat(response1)
+        response1 = (eta - hkt_now)
     except:
+        eta = 'N/A'
         response1 = 'N/A'
 
     try:
         response2 = response2['data'][0]['eta']
-        response2 = datetime.fromisoformat(response2)
-        response2 = (response2 - hkt_now)
+        eta = datetime.fromisoformat(response2)
+        response2 = (eta - hkt_now)
     except:
+        eta = 'N/A'
         response2 = 'N/A'
 
     try:
         response3 = response3['data'][0]['eta']
-        response3 = datetime.fromisoformat(response3)
-        response3 = (response3 - hkt_now)
+        eta = datetime.fromisoformat(response3)
+        response3 = (eta - hkt_now)
     except:
+        eta = 'N/A'
         response3 = 'N/A'
 
     try:
         response4 = response4['data'][0]['eta']
-        response4 = datetime.fromisoformat(response4)
-        response4 = (response4 - hkt_now)
+        eta = datetime.fromisoformat(response4)
+        response4 = (eta - hkt_now)
     except:
+        eta = 'N/A'
         response4 = 'N/A'
 
     try:
         response5 = response5['data'][0]['eta']
-        response5 = datetime.fromisoformat(response5)
-        response5 = (response5 - hkt_now)
+        eta = datetime.fromisoformat(response5)
+        response5 = (eta - hkt_now)
     except:
+        eta = 'N/A'
         response5 = 'N/A'
 
     try:
         response6 = response6['data'][0]['eta']
-        response6 = datetime.fromisoformat(response6)
-        response6 = (response6 - hkt_now)
+        eta = datetime.fromisoformat(response6)
+        response6 = (eta - hkt_now)
     except:
+        eta = 'N/A'
         response6 = 'N/A'
     
 
     data = {
-        '22x_OV': response1,
-        '22m_SFR': response2,
-        '22_SFR': response3,
-        '5R_SFR': response4,
-        '22d_SFR': response5,
-        '22x_KT': response6,
+        '22x_OV': [response1],
+        '22m_SFR': [response2],
+        '22_SFR': [response3],
+        '5R_SFR': [response4],
+        '22d_SFR': [response5],
+        '22x_KT': [response6],
     }
     return data
     
 
 def main():
-    st.title('Auto-Refreshing Data')
+    st.title('Come and Go')
     data = get_data()    
     st.markdown('# 22X走')
     st.markdown('# '+str(data['22x_OV']))
-    st.markdown('# '+str(data['22x_OV'])+"分鐘")
+    st.markdown('# '+str(data['22x_OV'])+" left")
     st.markdown('# 22M走')
     st.markdown('# '+str(data['22m_SFR']))
-    st.markdown('# '+str(data['22m_SFR'])+"分鐘")
+    st.markdown('# '+str(data['22m_SFR'])+" left")
     st.markdown('# 22走')
     st.markdown('# '+str(data['22_SFR']))
-    st.markdown('# '+str(data['22_SFR'])+"分鐘")
+    st.markdown('# '+str(data['22_SFR'])+" left")
     st.markdown('# 5R走')
     st.markdown('# '+str(data['22_SFR']))
-    st.markdown('# '+str(data['22_SFR'])+"分鐘")
+    st.markdown('# '+str(data['22_SFR'])+" left")
     st.markdown('# 22D走')
     st.markdown('# '+str(data['22d_SFR']))
-    st.markdown('# '+str(data['22d_SFR'])+"分鐘")
+    st.markdown('# '+str(data['22d_SFR'])+" left")
     st.markdown('# 22x返')
     st.markdown('# '+str(data['22x_KT']))
-    st.markdown('# '+str(data['22x_KT'])+"分鐘")
+    st.markdown('# '+str(data['22x_KT'])+" left")
 
 if __name__ == '__main__':
     main()
