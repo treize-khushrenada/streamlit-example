72b286f061ee68415890497ed3d9031ce4e4d298
Arthur C <78846238+treize-khushrenada@users.noreply.github.com>
2023-08-01 13:39:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,42 +18,42 @@ def get_data():
     try:
         response1 = response1['data'][0]['eta']
         response1 = datetime.fromisoformat(response1)
-        response1 = response1 - hkt_now
+        response1 = (response1 - hkt_now).round('1s')
     except:
         response1 = 'N/A'
 
     try:
         response2 = response2['data'][0]['eta']
         response2 = datetime.fromisoformat(response2)
-        response2 = response2 - hkt_now
+        response2 = (response2 - hkt_now).round('1s')
     except:
         response2 = 'N/A'
 
     try:
         response3 = response3['data'][0]['eta']
         response3 = datetime.fromisoformat(response3)
-        response3 = response3 - hkt_now
+        response3 = (response3 - hkt_now).round('1s')
     except:
         response3 = 'N/A'
 
     try:
         response4 = response4['data'][0]['eta']
         response4 = datetime.fromisoformat(response4)
-        response4 = response4 - hkt_now
+        response4 = (response4 - hkt_now).round('1s')
     except:
         response4 = 'N/A'
 
     try:
         response5 = response5['data'][0]['eta']
         response5 = datetime.fromisoformat(response5)
-        response5 = response5 - hkt_now
+        response5 = (response5 - hkt_now).round('1s')
     except:
         response5 = 'N/A'
 
     try:
         response6 = response6['data'][0]['eta']
         response6 = datetime.fromisoformat(response6)
-        response6 = response6 - hkt_now
+        response6 = (response6 - hkt_now).round('1s')
     except:
         response6 = 'N/A'
     
@@ -74,16 +74,22 @@ def main():
     data = get_data()    
     st.markdown('# 22X走')
     st.markdown('# '+str(data['22x_OV']))
+    st.markdown('# '+str(data['22x_OV'])+"分鐘")
     st.markdown('# 22M走')
     st.markdown('# '+str(data['22m_SFR']))
+    st.markdown('# '+str(data['22m_SFR'])+"分鐘")
     st.markdown('# 22走')
     st.markdown('# '+str(data['22_SFR']))
+    st.markdown('# '+str(data['22_SFR'])+"分鐘")
     st.markdown('# 5R走')
-    st.markdown('# '+str(data['5R_SFR']))
+    st.markdown('# '+str(data['22_SFR']))
+    st.markdown('# '+str(data['22_SFR'])+"分鐘")
     st.markdown('# 22D走')
     st.markdown('# '+str(data['22d_SFR']))
+    st.markdown('# '+str(data['22d_SFR'])+"分鐘")
     st.markdown('# 22x返')
     st.markdown('# '+str(data['22x_KT']))
+    st.markdown('# '+str(data['22x_KT'])+"分鐘")
 
 if __name__ == '__main__':
     main()
